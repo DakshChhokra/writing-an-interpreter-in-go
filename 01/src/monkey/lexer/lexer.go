e431fb93bcbc8f9c0d56b8c5b66477f454942c9a
daksh <50501229+DakshChhokra@users.noreply.github.com>
2025-09-25 23:52:47 -0500

    added Lexer type def, constructor an readChar helper

--- 01/src/monkey/lexer/lexer.go
+++ 01/src/monkey/lexer/lexer.go
@@ -1,8 +1,24 @@
-package lexer
-
 type Lexer struct {
+	input        string
+	position     int  //current char's positon
+	readPosition int  //current reading position(position + 1)
+	ch           byte //curretn char
 }
 
 func New(input string) *Lexer {
+	l := &Lexer{
+		input: input,
+	}
 
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0 //ASCII code for "NUL"
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
 }
